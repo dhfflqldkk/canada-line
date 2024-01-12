755c38a6249f2448c7e27da1e0fd42f254cb55d7
olivia <113372869+dhfflqldkk@users.noreply.github.com>
2024-01-11 16:37:45 -0800

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { MapContainer, TileLayer, Polyline, LatLngTuple } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import CircleMarks from './CircleMarks';
 
-const canadaLineToYVR: LatLngTuple[] = [
+const canadaLineToYVR = [
     [49.2859, -123.1116], // Waterfront
     [49.2829, -123.1192], // Vancouver City Centre
     [49.2745, -123.1217], // Yaletown-Roundhouse
@@ -19,7 +19,7 @@ const canadaLineToYVR: LatLngTuple[] = [
     [49.1939, -123.1786], // YVR-Airport
 ];
 
-const canadaLineToRichmond: LatLngTuple[] = [
+const canadaLineToRichmond = [
     [49.2859, -123.1116], // Waterfront
     [49.2829, -123.1192], // Vancouver City Centre
     [49.2745, -123.1217], // Yaletown-Roundhouse
